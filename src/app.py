166fe3d721d03abcfa0984a21b787dab0b800160
Kirankumar Sripati <krnsripati@gmail.com>
2020-06-01 20:44:23 +0530

    add default errors for api

--- src/app.py
+++ src/app.py
@@ -25,6 +25,52 @@ def create_app(test_config=None):
             'message': 'Hello World!'
         })
 
+    @app.errorhandler(AuthError)
+    def auth_error(e):
+        '''Error handler for AuthError'''
+        return jsonify({
+            'success': False,
+            'error': e.status_code,
+            'message': e.error
+        }), 401
+
+    @app.errorhandler(404)
+    def not_found(error):
+        '''Error handler for 404'''
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': get_error_message(error, 'resource not found')
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        '''Error handling for unprocessable entity'''
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': get_error_message(error, 'unprocessable')
+        }), 422
+
+    def get_error_message(error, default_message):
+        '''
+        Returns if there is any error message provided in
+        error.description.message else default_message
+        This can be passed by calling
+        abort(404, description={'message': 'your message'})
+
+        Parameters:
+        error (werkzeug.exceptions.NotFound): error object
+        default_message (str): default message if custom message not available
+
+        Returns:
+        str: Custom error message or default error message
+        '''
+        try:
+            return error.description['message']
+        except TypeError:
+            return default_message
+
     return app
 
 
