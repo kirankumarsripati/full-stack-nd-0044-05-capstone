acb9239b8199df767433246a8fa56ec5143173b2
Kirankumar Sripati <krnsripati@gmail.com>
2020-06-02 12:42:41 +0530

    add actors and movies endpoints

--- src/app.py
+++ src/app.py
@@ -3,7 +3,8 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from auth import AuthError
-from models import setup_db
+from models import setup_db, Actor, Movie
+from config import ITEMS_PER_PAGE
 
 
 def create_app(test_config=None):
@@ -22,12 +23,200 @@ def create_app(test_config=None):
                              'GET,PATCH,POST,DELETE,OPTIONS')
         return response
 
-    @app.route('/', methods=['GET'])
-    def index():
+    @app.route('/actors', methods=['GET'])
+    def get_actors():
+        page = request.args.get('page', 1, type=int)
+        selection = Actor.query.order_by(Actor.id).paginate(
+            page,
+            ITEMS_PER_PAGE,
+            False
+        )
+
+        selection_array = [actor.format() for actor in selection.items]
+
+        if len(selection_array) == 0:
+            abort(404, {'message': 'actors not found'})
+
         return jsonify({
             'success': True,
-            'message': 'Hello World'
+            'actors': selection_array,
+            'total': selection.total
         })
+
+    @app.route('/actors', methods=['POST'])
+    def create_actor():
+        body = request.get_json()
+
+        if not body:
+            abort(422, {'message': 'invalid body JSON'})
+
+        name = body.get('name', None)
+        age = body.get('age', None)
+        gender = body.get('gender', 'Other')
+
+        if not name:
+            abort(422, {'message': 'name cannot be blank'})
+
+        if not age:
+            abort(422, {'message': 'age cannot be blank'})
+
+        try:
+            actor = Actor(
+                name=name,
+                age=age,
+                gender=gender
+            )
+            actor.insert()
+
+        except Exception:
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'created': actor.id,
+        })
+
+    @app.route('/actors/<int:actor_id>', methods=['PATCH'])
+    def patch_actor(actor_id):
+        body = request.get_json()
+
+        if not body:
+            abort(422, {'message': 'invalid body JSON'})
+
+        actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+        if not actor:
+            abort(404, {'message': 'actor not found'})
+
+        try:
+            actor.name = body.get('name', actor.name)
+            actor.age = body.get('age', actor.age)
+            actor.gender = body.get('gender', actor.gender)
+
+            actor.update()
+        except Exception:
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'updated': actor.id,
+        })
+
+    @app.route('/actors/<int:actor_id>', methods=['DELETE'])
+    def delete_actor(actor_id):
+        actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+        if not actor:
+            abort(404, {'message': 'actor not found'})
+
+        try:
+            actor.delete()
+        except Exception:
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'deleted': actor_id,
+        })
+
+    @app.route('/movies', methods=['GET'])
+    def get_movies():
+        page = request.args.get('page', 1, type=int)
+        selection = Movie.query.order_by(Movie.id).paginate(
+            page,
+            ITEMS_PER_PAGE,
+            False
+        )
+
+        if len(selection.items) == 0:
+            abort(404, {'message': 'movies not found'})
+
+        selection_array = [movie.format() for movie in selection.items]
+
+        return jsonify({
+            'success': True,
+            'movies': selection_array,
+            'total': selection.total
+        })
+
+    @app.route('/movies', methods=['POST'])
+    def create_movies():
+        body = request.get_json()
+
+        if not body:
+            abort(422, {'message': 'invalid body JSON'})
+
+        title = body.get('title', None)
+        release_year = body.get('release_year', None)
+
+        if not title:
+            abort(422, {'message': 'title cannot be blank'})
+
+        if not release_year:
+            abort(422, {'message': 'release_year cannot be blank'})
+
+        try:
+            movie = Movie(
+                title=title,
+                release_year=release_year
+            )
+            movie.insert()
+        except Exception:
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'created': movie.id,
+        })
+
+    @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+    def patch_movie(movie_id):
+        body = request.get_json()
+
+        if not body:
+            abort(422, {'message': 'invalid body JSON'})
+
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+        if not movie:
+            abort(404, {'message': 'actor not found'})
+
+        try:
+            movie.title = body.get('title', movie.title)
+            movie.release_year = body.get('release_year', movie.release_year)
+        except Exception:
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'updated': movie.id,
+        })
+
+    @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+    def delete_movie(movie_id):
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+        if not movie:
+            abort(404, {'message': 'movie not found'})
+
+        try:
+            movie.delete()
+        except Exception:
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'deleted': movie_id
+        })
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        '''Error handler for bad request'''
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'bad request'
+        }), 400
 
     @app.errorhandler(AuthError)
     def auth_error(e):
