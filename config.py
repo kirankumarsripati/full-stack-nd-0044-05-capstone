8e38728a8632d18ccfd83e93886431454ca906ff
Kirankumar Sripati <krnsripati@gmail.com>
2020-06-03 11:35:31 +0530

    remove src folder

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 ITEMS_PER_PAGE = 10
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
